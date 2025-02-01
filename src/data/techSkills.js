80ad3bc30cfa7dd6e98169d7f2fe44496440d994
Kevin Luis <kevluis10@gmail.com>
2025-02-01 13:32:16 -0600

    Updated skill list and level of expertise

--- src/data/techSkills.js
+++ src/data/techSkills.js
@@ -11,36 +11,51 @@ const techSkills = [
   },
   {
     id: 3,
-    title: "Node Js",
+    title: "AngularJS",
     percentage: 90,
   },
   {
     id: 4,
+    title: "Angular",
+    percentage: 90,
+  },
+  {
+    id: 5,
+    title: "Chrome Ext",
+    percentage: 95,
+  },
+  {
+    id: 6,
+    title: "Node Js",
+    percentage: 90,
+  },
+  {
+    id: 7,
     title: "Nest Js",
     percentage: 60,
   },
   {
-    id: 5,
+    id: 8,
     title: "Firebase",
     percentage: 70,
   },
   {
-    id: 6,
+    id: 9,
     title: "NoSQL",
     percentage: 80,
   },
   {
-    id: 7,
+    id: 10,
     title: "SQL",
-    percentage: 60,
+    percentage: 90,
   },
   {
-    id: 8,
+    id: 11,
     title: "AWS",
     percentage: 80,
   },
   {
-    id: 9,
+    id: 12,
     title: "GCP",
     percentage: 50,
   },
